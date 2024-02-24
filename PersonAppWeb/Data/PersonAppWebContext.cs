0e95ac7c0f94428937d66b3851abd3c5c9e59e1e
rexpadrinas <62008443+rexpadrinas@users.noreply.github.com>
2024-02-24 12:58:43 +0800

    File upload part 3 - Data

---
+++ PersonAppWeb/Data/PersonAppWebContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonAppWeb.Models;
+
+namespace PersonAppWeb.Data
+{
+    public class PersonAppWebContext : DbContext
+    {
+        public PersonAppWebContext (DbContextOptions<PersonAppWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersonAppWeb.Models.PersonType> PersonType { get; set; } = default!;
+
+        public DbSet<PersonAppWeb.Models.PersonViewModel> PersonViewModel { get; set; } = default!;
+    }
+}
