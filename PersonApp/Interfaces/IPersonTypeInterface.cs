45a9d4907c22365c34a509484722cbea84c875b4
rexpadrinas <62008443+rexpadrinas@users.noreply.github.com>
2024-02-24 13:52:42 +0800

    file upload 16 - personapp 5

---
+++ PersonApp/Interfaces/IPersonTypeInterface.cs
@@ -0,0 +1,15 @@
+﻿using PersonApp.Model;
+
+namespace PersonApp.Interfaces
+{
+    public interface IPersonTypeInterface
+    {
+        ICollection<PersonType> GetPersonTypes();
+        PersonType GetPersonType(int id);
+        PersonType CreatePersonType(PersonType persontype);
+        PersonType UpdatePersonType(int id, PersonType oldpersontype, PersonType newpersontype);
+        bool DeletePersonType(int id);
+
+
+    }
+}
